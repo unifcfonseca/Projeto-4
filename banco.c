7d6ffaf2309631487e47df60e0dbfc274add22bb
pedroosabainsk <160777874+pedroosabainsk@users.noreply.github.com>
2024-04-30 20:48:16 +0000

    Listar

--- banco.c
+++ banco.c
@@ -5,7 +5,19 @@ ERROS DeletarCliente(){
   
 }
 ERROS ListarClientes(){
-  
+  if (*pos == 0){
+    return SEM_CLIENTES;
+  }
+
+  for (int i = 0; i < *pos; i++){
+    printf("Posição: %d\n", i+1);
+    printf("Nome: %s\t", banco[i].nome);
+    printf("CPF: %ld\n", banco[i].cpf);
+    printf("Tipo de Conta: %s\t", banco[i].tipo);
+    printf("Saldo: %s\n\n", banco[i].saldo); 
+  }
+
+  return OK;
 }
 ERROS Debito(){
 
