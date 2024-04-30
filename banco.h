8c821f07dbbeb82ee5b646878b90c06dd5fa9b3e
unifcfonseca <163344346+unifcfonseca@users.noreply.github.com>
2024-04-30 20:41:57 +0000

    Função de criar

--- banco.h
+++ banco.h
@@ -1,13 +1,13 @@
-typedef enum {OK, MAX_CLIENTES, SEM_CLIENTES, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERROS;
+typedef enum {OK, MAX_CLIENTES, SEM_CLIENTES, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER, CLIENTE_EXISTENTE} ERROS;
 
 typedef ERROS (*funcao)();
 
-ERROS NovoCliente();
-ERROS DeletarCliente();
-ERROS ListarClientes();
-ERROS Debito();
-ERROS Deposito();
-ERROS Transferencia();
-ERROS Extrato();
-ERROS Salvar();
-ERROS Carregar();
+ERROS NovoCliente(Banco *banco, int *pos);
+ERROS DeletarCliente(Banco *banco, int *pos);
+ERROS ListarClientes(Banco *banco, int *pos);
+ERROS Debito(Banco *banco, int *pos);
+ERROS Deposito(Banco *banco, int *pos);
+ERROS Transferencia(Banco *banco, int *pos);
+ERROS Extrato(Banco *banco, int *pos);
+ERROS Salvar(Banco *banco, int *pos);
+ERROS Carregar(Banco *banco, int *pos);
